6bdac604678c2837999f4352d2466146de5fd06e
william chang <wwwy3y3@gmail.com>
2017-08-04 17:59:14 +0800

    multiroutes support for middleware render

--- server/coren-middleware.js
+++ server/coren-middleware.js
@@ -1,18 +1,33 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
 const {has} = require('lodash');
+const path = require('path');
+
+const getEntryHtml = entry => {
+  const filePath = path.join('./.coren/public/', `${entry}.html`);
+  return fs.readFileSync(filePath, 'utf8');
+};
 
 module.exports = function() {
-  const htmls = {};
-  const assets = JSON.parse(fs.readFileSync('./.coren/assets.json', 'utf8'));
-  Object.keys(assets).forEach(function(asset) {
-    htmls[asset] = fs.readFileSync('./.coren/public/' + asset + '.html', 'utf8');
-  });
   return function corenMiddleware(req, res, next) {
     res.sendCoren = function(entry, options) {
+      // multi routes render
+      // from index/users/1 => users/index/1
+      if (entry.indexOf('/') >= 0) {
+        const entries = entry.split('/').filter(val => val);
+
+        // multi routes render
+        if (entries.length > 1) {
+          const firstEntry = entries[0];
+          const newEntries = entries.slice(1);
+          newEntries.push(firstEntry);
+          entry = newEntries.join('/');
+        }
+      }
+
       // merge preloaded state
       if (options.updatePreloadedState) {
-        const $ = cheerio.load(htmls[entry]);
+        const $ = cheerio.load(getEntryHtml(entry));
         $('script').get().forEach(function(script) {
           // find script with `data-coren` attr
           if (has(script.attribs, 'data-coren')) {
@@ -24,7 +39,7 @@ module.exports = function() {
         });
         res.send($.html());
       } else {
-        res.send(htmls[entry]);
+        res.send(getEntryHtml(entry));
       }
     };
     next();
